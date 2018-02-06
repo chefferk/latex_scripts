1cb5d79aad163441a7dfb0ba7e498ac7b2683478
keaton <chefferk@tamu.edu>
2018-02-05 23:43:15 -0600

    added latex_swap

--- latex_to_logic.py
+++ latex_to_logic.py
@@ -26,12 +26,30 @@ def latex_print(list):
         assumption_set.append(list[i][0])
         line_number.append(list[i][1])
         sentence.append(list[i][2])
-        assumption.append(list[i][3])
+        assumption.append(list[i][3] + list[i][-1])
 
         print('{} & {} ${}$ & {}\\\\' .format(assumption_set[i], line_number[i], sentence[i], assumption[i]))
 
 
+def latex_swap(convertions, list):
+    for i in range(len(list)):
+        for j in range(len(convertions)):
+            list[i] = [x.replace(convertions[j][2], convertions[j][1]) for x in list[i]]
+
+        print(list[i][2])
+
+    return list
+
+
 def main():
+    convertions = [['not', ' \\neg ', '~'],
+                   ['and', ' \\wedge ', '&'],
+                   ['or', ' \\vee ', 'v'],
+                   ['implies', ' \\to ', '->'],
+                   ['iff', ' \\leftrightarrow ', '<->'],
+                   ['all', ' \\forall ', '@'],
+                   ['some', ' \\exists ', '$']]
+
     if test_mode is True:
         infile = open('latex.txt', 'r')
     else:
@@ -53,6 +71,9 @@ def main():
         print('-' * 20)
 
     latex_print(split_list)
+    print('-' * 20)
+    swap_list = latex_swap(convertions, split_list)
+    latex_print(swap_list)
 
 
 if __name__ == "__main__":
