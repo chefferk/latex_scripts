377e7a71677ea14ac2015409a1cb3ede268a09bc
keaton <chefferk@tamu.edu>
2018-02-05 22:19:36 -0600

    split list and txt files

--- latex_to_logic.py
+++ latex_to_logic.py
@@ -22,7 +22,30 @@ def main():
     else:
         infile = get_file()
 
-    print(infile)
+    content_list = infile.readlines()
+    infile.close()
+
+    # Option 1) individual
+    line_number = list()
+    assumption_set = list()
+    sentence = list()
+    annotation = list()
+
+    for i in range(len(content_list)):
+        assumption_set.append(re.findall(r'^\S*', content_list[i]))
+        line_number.append(re.findall(r'\(\d\)', content_list[i]))
+        sentence.append(re.findall(r'', content_list[i]))
+        annotation.append(re.findall(r'', content_list[i]))
+
+    # Optain 2) all at once
+    split_list = list()
+
+    for i in range(len(content_list)):
+        split_list.append(re.findall(r'([^\s]+)', content_list[i]))
+
+    for i in split_list:
+        print(i)
 
 
-main()
+if __name__ == "__main__":
+    main()
