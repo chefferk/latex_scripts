cd9c6587747aac45ad3403339099c3e8493ca08a
keaton <chefferk@tamu.edu>
2018-02-06 14:06:20 -0600

    reorganized

--- latex_to_logic.py
+++ latex_to_logic.py
@@ -2,12 +2,19 @@ import re
 from logic_suplemental import convertions, rules
 import pprint
 
-pprint.pprint(convertions)
-pprint.pprint(rules)
-print('asdf')
 # If in test mode, file is already selected vs user entering file name
 # DEBUG
 test_mode = True
+
+
+def menu():
+    # DEBUG
+    if test_mode is True:
+        choice = 1
+    else:
+        print('(1) Logic to LaTex')
+        print('(2) LaTex to Logic\n')
+        choice = input('Option: ')
 
 
 # If test mode is False, this will ask user for file name and loop until valid file is given
@@ -23,15 +30,23 @@ def get_file():
     return infile
 
 
-def menu():
-    print('(1) Logic to LaTex')
-    print('(2) LaTex to Logic\n')
+def split_list(list):
+    # Split elements in list
+    new_list = []
+    for i in range(len(list)):
+        new_list.append(re.findall(r'([^\s]+)', list[i]))
 
     # DEBUG
     if test_mode is True:
-        choice = 1
-    else:
-        choice = input('Option: ')
+        print('-' * 20)
+        print('Split elements')
+        print('[Assumption set, Line Number, Sentence, Annotation]')
+        print('-' * 20)
+        # [Assumption set, Line Number, Sentence, Annotation]
+        for i in new_list:
+            print(i)
+
+        return new_list
 
 
 # Prints LaTex formating to the console
@@ -55,55 +70,30 @@ def latex_print(list):
             assumption_req.append('')
             assumption.append(list[i][3])
 
-        # Prints to console for right now, could return or could save to file in future
+        # TODO(add): Prints to console for right now, could return or could save to file in future
         print('{} & {} ${}$ & {} & {}\\\\' .format(assumption_set[i], line_number[i], sentence[i], assumption_req[i], assumption[i]))
 
 
 # Swaps symbols mainly for the sentence column, but I'm having trouble just changing that column
 def sentence_swap(convertions, list):
-    if test_mode is True:
-        print('-' * 20)
-        print('Sentences after convertion')
-        print('-' * 20)
-
     # TODO(fix) : only want this to apply to list[i][2]
     for i in range(len(list)):
         for j in range(len(convertions)):
             list[i] = [x.replace(convertions[j][2], convertions[j][1]) for x in list[i]]
-
-        # DEBUG
-        if test_mode is True:
-            print(list[i][2])
 
     return list
 
 
 # Swaps assumption symbols for English words, they still suck. Will need to convert again to our definitions
 def assumption_swap(rules, list):
-    if test_mode is True:
-        print('-' * 20)
-        print('Assumption after convertion')
-        print('-' * 20)
-
     for i in range(len(list)):
         for j in range(len(rules)):
             list[i] = [x.replace(rules[j][0], rules[j][2]) for x in list[i]]
-
-        # DEBUG
-        if test_mode is True:
-            print(list[i][2])
 
     return list
 
 
 def main():
-    convertions = [['not', ' \\neg ', '~'],
-                   ['and', ' \\wedge ', '&'],
-                   ['or', ' \\vee ', 'v'],
-                   ['implies', ' \\to ', '->'],
-                   ['iff', ' \\leftrightarrow ', '<->'],
-                   ['all', ' \\forall ', '@'],
-                   ['some', ' \\exists ', '$']]
 
     menu()
 
@@ -113,56 +103,38 @@ def main():
     else:
         infile = get_file()
 
+    # Create list and close file
     content_list = infile.readlines()
     infile.close()
 
-    # Get rules file
-    # This gets really confusing, clean this up
-    rules_infile = open('rules2.txt', 'r')
-    rules = rules_infile.readlines()
-    rules_infile.close()
-
-    # DEBUG
-    this = True
-    if test_mode is True and this == False:
-        pprint.pprint(rules)
-
-    # strips & splits lines -> makes 2d array
-    rules_list = list()
-    for i in rules:
-        content = i.strip()
-        rules_list.append(content.split(', '))
+    # Split elements in list
+    content_list = split_list(content_list)
 
     # DEBUG
     if test_mode is True:
-        pprint.pprint(rules_list)
-
-    split_list = list()
-
-    for i in range(len(content_list)):
-        split_list.append(re.findall(r'([^\s]+)', content_list[i]))
-
-    # DEBUG
-    if test_mode is True:
-        # [Assumption set, Line Number, Sentence, Annotation]
-        for i in split_list:
-            print(i)
-
         print('-' * 20)
-
-    # DEBUG
-    if test_mode is True:
-        latex_print(split_list)
+        print('Initial LaTex print before any swaps')
         print('-' * 20)
+        latex_print(content_list)
 
-    swap_list = assumption_swap(rules_list, split_list)
+    content_list = assumption_swap(rules, content_list)
 
     # DEBUG
     if test_mode is True:
-        latex_print(swap_list)
+        print('-' * 20)
+        print('LaTex print after Annotations are swapped')
+        print('-' * 20)
+        latex_print(content_list)
 
-    swap_list = sentence_swap(convertions, split_list)
-    latex_print(swap_list)
+    content_list = sentence_swap(convertions, content_list)
+
+    if test_mode is True:
+        print('-' * 20)
+        print('LaTex print after sentences are swapped (final result)')
+        print('-' * 20)
+        latex_print(content_list)
+    else:
+        latex_print(content_list)
 
 
 if __name__ == "__main__":
