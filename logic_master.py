b1e27c84ea9c3698521f74531942a2a9259cb269
keaton <chefferk@tamu.edu>
2018-02-08 11:39:30 -0600

    added menu in master

--- logic_master.py
+++ logic_master.py
@@ -1,23 +1,45 @@
 from logic_suplemental import *
 from latex_to_logic import *
-from logic_to_latex import *
+#from logic_to_latex import *
+
+# DEBUG
+test_mode = True
 
 
 def menu():
-    # DEBUG
-    if test_mode is True:
-        choice = 1
-    else:
+    flag = True
+    while flag is True:
         print('(1) Logic to LaTex')
         print('(2) LaTex to Logic\n')
-        choice = input('Option: ')
+        choice1 = input('Option: ')
 
-    return choice
+        if choice == 1:
+            print('(1) Proof')
+            print('(2) Single line')
+            choice2 = input('Option: ')
+            flag = False
+
+        elif choice == 2:
+            print('(1) Proof')
+            print('(2) Single line')
+            choice2 = input('Option: ')
+            flag = False
+
+        else:
+            print('Invalid input please try again')
+
+        return choice1, choice2
 
 
 def main():
 
-    choice = menu()
+    if test_mode is True:
+        choice1 = 1
+        choice2 = 1
+    else:
+        choice1, choice2 = menu()
+
+    print(choice1, choice2)
 
 
 if __name__ == "__main__":
